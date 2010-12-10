675dfd56685dce1ca33a98a3fcf176528efe8395
Sidney Lima Filho <sidney.filho@vivina.com.br>
2010-12-10 18:58:35 -0200

    Adicionada a rotina de template dentro do smartclient

--- jquery.smartclient.js
+++ jquery.smartclient.js
@@ -49,6 +49,62 @@
             }
 
         },
+        render: function(data) {
+            if (this.size() == 0) throw new Error("Zero element selected!");
+
+            var template = this.html();
+
+            // Convert the template into pure JavaScript
+            var script = "var p=[], print=function(){ p.push.apply(p,arguments); }; " +
+            "dataItem = dataItem || [];" +
+            "with(dataItem){" +
+            "   p.push('" + template // Introduce the data as local variables using with(){}
+                    .split("&lt;").join("<")
+                    .split("%3C").join("<")
+                    .split("&gt;").join(">")
+                    .split("%3E").join(">")
+                    .split("&quot;").join('"')
+                    .replace("<!--", " ")
+                    .replace("-->", " ")
+                    .replace(/'/g, "\\'")
+                    .replace(/[\r\t\n]/g, " ")
+
+                   .replace(/'(?=[^$]*$>)/g, "\t")
+
+                   .split("\t").join("'")
+                   .replace(/<\$=(.+?)\$>/g, "',$1,'")
+
+                   .split("<$").join("');")
+                   .split("$>").join("p.push('")
+
+          + "');}return p.join('');";
+
+            try {
+                $.cache = $.cache || {};
+                // Generate a reusable function that will serve as a template
+                // generator (and which will be cached).
+                //var fn = !/\W/.test(this.id) ? cache[this.id] = (cache[this.id] || $(this).template()) :
+                var fn = $.cache[template] || ($.cache[template] = new Function("dataItem", script));
+
+                var html = "";
+                if ($.isArray(data)) {
+                    for (var i = 0; i < data.length; i++) html += fn(data[i]);
+                } else {
+                    html += fn(data);
+                }
+
+            } catch (err) {
+                $("<pre class='error' />").text("The template is mal-formed, because " + err + "\n\n" + script).appendTo("body");
+            }
+
+
+            // Provide some basic currying to the user
+            //var html = fn(data);
+            //this.html(html);
+            return data ? html : fn;
+
+
+        },
         ajaxIframe: function(url, ctrl, onsucess) {
             var iframe = $("#ajaxIFrame");
 
@@ -201,15 +257,10 @@
 
                                         html = tpl.render(data);
                                     }
-
                                 }
-
-                                $this.attachHtmlInTarget(html, target);
-
                             }
 
-
-
+                            $this.attachHtmlInTarget(html, target);
                         }
 
                         if (options.onsucess)
@@ -237,9 +288,12 @@
 
         initializeControls: function() {
 
+
             this._initializeCommandControls();
 
             this._initializeCalendars();
+
+            this._initializeThemeStyle();
 
             this._initializeHtmlBox();
 
@@ -247,7 +301,7 @@
 
             this._initializeLightboxEvolution();
 
-            this._initializeThemeStyle();
+
 
             return this;
 
@@ -314,9 +368,7 @@
 
                     $(this).datepicker(dateOptions);
 
-                    $(this).setMask({
-                        mask: "39/19/9999"
-                    });
+                    $(this).setMask({ mask: "39/19/9999" });
                 }
             });
 
@@ -503,3 +555,4 @@ function PageNotFoundException(sender) {
 function TargetMissingException(sender) {
     Exception(" Não foi encontrado o elemento html '" + sender.attrUp("target") + "'! \n\n Html Trace: " + sender.outerHtml());
 }
+
