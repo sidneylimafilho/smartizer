dff5d5dd70dcbd5d91935933be27a66781611ebe
Sidney Lima Filho <sidney.filho@vivina.com.br>
2012-03-23 20:45:23 -0300

    Add site sample source

---
+++ App_Code/AppDatabase.partial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+
+
+public partial class AppDatabase : System.Data.Linq.DataContext
+{	
+	public AppDatabase() : base("Data Source=|DataDirectory|\\AppDatabase.sdf")
+	{
+		OnCreated();
+	}	
+}
+
+[Serializable]
+[System.Data.Services.Common.DataServiceKey("Id")]
+public partial class Users {}
+
